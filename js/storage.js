2e880d9a7838ef24c1cb999e6a80dc9c4d07891b
peet <teilzeitjobbewerber@gmx.de>
2024-08-19 09:20:10 +0200

    refactor: renaming pngs

--- js/storage.js
+++ js/storage.js
@@ -1,7 +1,8 @@
 // Tests for remote storage
 
 // generate tokens at  https://remote-storage.developerakademie.org/token-generator
-const STORAGE_TOKEN = '6OT5NHLEZTIV1ASE5C02UCNNGQZH7TEB6HQPFP5F';
+const STORAGE_TOKEN = '6OT5NHLEZTIV1ASE5C02UCNNGQZH7TEB6HQPFP5F'; // Original Token
+// const STORAGE_TOKEN = '6OT5NHLEZTIV1ASE5C02UCNNGQZH7TEB6HQPAA3A'; // New Token
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 /**
@@ -13,6 +14,17 @@ async function setItem(key, value) {
     .then(res => res.json());
 }
 
+async function getRemote() {
+  let myItems = await getItem("board_key");
+  let parsedItem = JSON.parse(myItems);
+  parsedItem.shift()
+  console.log(parsedItem);
+  setItem("board_key", parsedItem)
+  
+}
+
+// getRemote();
+
 /**
  * Takes a key, creates the right url
  * returns response as a json
@@ -20,9 +32,9 @@ async function setItem(key, value) {
 async function getItem(key) {
   const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
   return fetch(url).then(res => res.json()).then(res => {
-      if (res.data) { 
-          return res.data.value;
-      } throw `Could not find data with key "${key}".`;
+    if (res.data) {
+      return res.data.value;
+    } throw `Could not find data with key "${key}".`;
   });
 }
 
